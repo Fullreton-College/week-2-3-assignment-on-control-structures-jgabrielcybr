959471080a713363b7aa9d0b23e55f07987cf2ed
jgabrielcybr <hollowfx35@gmail.com>
2026-02-21 22:11:39 -0800

    Update Assignment1_RectangleArea.cpp

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -2,9 +2,36 @@
 
 using namespace std;
 
-int main()
+int main ()
 {
-	
+    double lengthOne, lengthTwo, widthOne, widthTwo;
+
+    cout<<"Rectangle Area Comparison "<<endl;
+    cout<<"Enter the length of the first rectangle "<<endl;
+    cin>>lengthOne;
+
+    cout<<"Enter the width of the first rectangle "<<endl;
+    cin>>widthOne;
+
+    cout<<"Enter the length of the second rectangle "<<endl;
+    cin>>lengthTwo;
+
+    cout<<"Enter the width of the second rectangle "<<endl;
+    cin>>widthTwo;
+
+    if ( (lengthOne * widthOne) > (lengthTwo * widthTwo) )
+    {
+        cout<<"The area of the first rectangle is "<<lengthOne * widthOne<<endl;
+        cout<<"The area of the second rectangle is "<<lengthTwo * widthTwo<<endl;
+        cout<<"The area of the first rectangle is greater than that of the second"<<endl;
+    }
+    
+    else 
+    {   
+        cout<<"The area of the first rectangle is "<<lengthOne * widthOne<<endl;
+        cout<<"The area of the second rectangle is "<<lengthTwo * widthTwo<<endl;
+        cout<<"The area of the second rectangle is greater than that of the first"<<endl;
+    }
+
     return 0;
 }
-
